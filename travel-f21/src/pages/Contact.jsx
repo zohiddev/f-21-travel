fbf30184122b296bbf81cba0dabed6f2ce00811b
gangster <mezarcisecret0305@gmail.com>
2024-01-31 18:20:06 +0500

    add all project

--- travel-f21/src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Contact() {
-  return (
-    <div className='container'>Contact</div>
-  )
-}
-
-export default Contact
--- travel-f21/src/pages/VisaPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function VisaPage() {
-  return (
-    <div className='container'>VisaPage</div>
-  )
-}
-
-export default VisaPage
